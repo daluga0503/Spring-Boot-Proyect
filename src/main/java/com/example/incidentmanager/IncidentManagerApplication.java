630e28e990e02f65b20a40393866820c19fb2bcf
daluga0503 <daniel.luquegallardo@gmail.com>
2024-02-19 10:29:02 +0100

    commandLinerunner implemented

--- src/main/java/com/example/incidentmanager/IncidentManagerApplication.java
+++ src/main/java/com/example/incidentmanager/IncidentManagerApplication.java
@@ -1,13 +1,32 @@
 package com.example.incidentmanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.incidentmanager.User.domain.UserEntity;
+import com.example.incidentmanager.User.service.UserService;
+
 @SpringBootApplication
-public class IncidentManagerApplication {
+public class IncidentManagerApplication implements CommandLineRunner{
+	private final UserService service;
+
+	public IncidentManagerApplication(UserService service){
+		this.service = service;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IncidentManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		UserEntity admin = new UserEntity(0, "Daniel", "Luga", "daniel@gmail.com", "2ºDam", "ADMIN", "123456");
+		try {
+			this.service.create(admin);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
--- src/main/java/com/example/incidentmanager/Incidents/domain/IncidentEntity.java
+++ src/main/java/com/example/incidentmanager/Incidents/domain/IncidentEntity.java
@@ -5,7 +5,6 @@ import com.example.incidentmanager.User.domain.UserEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
